573c8523d2df581f9521744af4e2cff198fa9c05
saeid <saeidnobahari1@gmail.com>
2023-08-24 16:26:13 -0700

    public project

---
+++ src/modules/Home/sections/Worked/config/constants.ts
@@ -0,0 +1,55 @@
+import { WorkedTypes } from './types';
+
+// worked data
+export const worked_data : WorkedTypes[] = [
+   { 
+      id: 0,
+      label : "Ciklum",
+      description : [
+         "I developed the various web apps using React and Node.js.",
+         "Also, I developed some chrome extensions and 3D configurators using React, three.js and GSAP.",
+      ],
+      deadline : "Apr 2021 - Oct 2022",
+      title: "Full-Stack Developer",
+      company : "Ciklum",
+      url : "#",
+   },
+   { 
+      id: 1,
+      label : "Ciklum",
+      description : [
+        "I worked as a lead developer on the SaaS project.",
+        "In this project, I used Django Rest Framework to develop the back-end and developed some part of front-end using React, TypeScript and Tailwind CSS",
+      ],
+      deadline : "May 2018 - Mar 2021",
+      title: "Lead developer",
+      company : "Ciklum",
+      url : "#",
+   },
+   // { 
+   //    id: 2,
+   //    label : "Fiverr",
+   //    description : [
+   //      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+   //      "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+   //      "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+   //    ],
+   //    deadline : "Aug 21 - present",
+   //    title: "Full stack developer",
+   //    company : "Fiverr",
+   //    url : "https://fiverr.com/raihanhosen001",
+   // },
+   // { 
+   //    id: 3,
+   //    label : "Upwork",
+   //    description : [
+   //      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+   //      "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+   //      "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+   //    ],
+   //    deadline : "Aug 22 - present",
+   //    title: "Full stack developer",
+   //    company : "Upwork",
+   //    url : "https://www.upwork.com/freelancers/~010b0f3e278bfac59f",
+   // },
+]
---
+++ src/shared/config/constants.ts
@@ -0,0 +1,104 @@
+import classNames from "classnames";
+import { ProjectTypes } from "./types";
+
+// whenever wee need to change default className value we will use this
+export const cx = classNames;
+
+// nav data
+export const navData = [ "About", "Experience", "Works", "Contact" ]
+
+// project data
+export const projects : ProjectTypes[] = [
+    {
+        title : "HR SaaS Platform",
+        description : "This platform is a community of business and HR leaders, HR practitioners, technologists, entrepreneurs, consultants.",
+        tech : [ "React", "Tailwind CSS", "Node.js", "Express.js", "MongoDB" ],
+        code : 'https://github.com/oleksandrStar',
+        live : 'https://www.hackinghrlab.io/home',
+        thumnail : "HR.png",
+        featured : true
+    },
+
+    {
+        title : "DG Team",
+        description : "'Dg team' is an company management system. I use this system for front end “Next js, Tailwind CSS, Axios, Redux, Redux-thunk, Formik, Yup, react-query” and for Backend “Node js, Express js, MongoDB, Socket.io, webRtc”",
+        tech : [ "Next.js", "Tailwind CSS", "Redux", "Node.js" ],
+        live : 'https://dgteam.vercel.app/',
+        thumnail : "dg-team.png",
+        featured : true
+    },
+
+    {
+        title : "Safebear",
+        description : "SafeBear is a cross-platform solution that protects its users from internet toxicity using blockchain technology in the web3",
+        tech : [ "Sveltekit", "Tailwind CSS", "Web3", "Supabase" ],
+        live : 'https://www.safebear.com',
+        code: 'https://github.com/oleksandrStar/safebear-network',
+        thumnail : "safebear.png",
+        featured : true
+    },
+
+    {
+        title : "AI blog Platform",
+        description : "This is an AI blog platform was developed using Next.js, Editor.js, OpenAI, ChatGPT, HyGraph and Cloudinary",
+        tech : [ "Next.js", "Editor.js", "ChatGPT", "HyGraph" ],
+        live : 'https://ai-blog.com',
+        code: 'https://github.com/oleksandrStar',
+        thumnail : "aiblog.png",
+        featured : true
+    },
+
+    {
+        title : "E-Commerce Site",
+        description : "This is an e-commerce platform was developed using React and Node.js. In this project, I used MongoDB for a database.",
+        tech : [ "React.js", "Redux", "Node.js", "Stripe" ],
+        code : 'https://github.com/oleksandrStar',
+        live : 'https://github.com/oleksandrStar',
+        thumnail : "ecommer.png"
+    },
+
+    {
+        title : "Careerly Web App",
+        description : "This is a careerly web app to manage the team. You can use this app to get reports at a team level to derive insights into your own performance as a menager.",
+        tech : [ "React.js", "Tailwind CSS", "Node.js", "Firebase" ],
+        live : 'https://www.careerly.app/',
+        code: "https://github.com/oleksandrStar/careerly",
+        thumnail : "careerlyweb.png"
+    },
+
+    {
+        title : "Careerly Chrome Extension",
+        description : "This is a careerly chrome extension to manage the team. You can use this extension to report the progress at a team level to derive insights into your own performance as a menager.",
+        tech : [ "React.js", "Tailwind CSS", "Firebase", "MV3" ],
+        live : 'https://chrome.google.com/webstore/detail/careerly/hggmhgodcnehgieapofeakhkogikkhkl/',
+        code: 'https://github.com/oleksandrStar/careerly',
+        thumnail : "careerlycextension.png"
+    },
+
+    {
+        title : "Elixr Landing Page",
+        description : "This is a landing page project was developed using GSAP, React and Tailwind CSS. I implemented the 3D animation, text animation and scroll animation using GSAP.",
+        tech : [ "GSAP", "React.js", "Tailwind CSS" ],
+        live : 'https://www.elixr.earth/',
+        code: "https://github.com/oleksandrStar/elixr-webgl",
+        thumnail : "elixr.png"
+    },
+
+    {
+        title : "Facility Management System",
+        description : "I developed the Facility Management System using React, Node, D3.j and MongoDB. In this project, I implemented the floorplan using d3.js.",
+        tech : [ "React.js", "Tailwind CSS", "Firebase", "MV3" ],
+        live : 'https://github.com/oleksandrStar/facility-management-system',
+        code: 'https://github.com/oleksandrStar/facility-management-system',
+        thumnail : "floorplan.png"
+    },
+
+    {
+        title : "3D Configurator Website",
+        description : "I developed the 3D configurator website to emulate the room with colors, materials and so on.",
+        tech : [ "React.js", "Three.js", "WebGL", "Tailwind CSS" ],
+        live : 'https://github.com/oleksandrStar',
+        code: 'https://github.com/oleksandrStar',
+        thumnail : "3dconf.png"
+    }
+]
